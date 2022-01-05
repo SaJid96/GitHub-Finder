04e7e5586bb889165c19d08a43fd0a2bcc27b6f3
Sajid Hussain <sajidhussain98@gmail.com>
2022-01-06 00:16:49 +0530

    modified links in  Navbar component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,15 +6,22 @@ function Navbar({title}) {
     return (
       <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
         <div className="container mx-auto">
-            <div className="flex-none px-2 mx-2">
-                <FaGithub className='inline pr-2 text-3xl'/>
-                <Link to='/' className='align-middle text-lg font-bold'>{title}</Link>
+          <div className="flex-none px-2 mx-2">
+            <FaGithub className="inline pr-2 text-3xl" />
+            <Link to="/" className="align-middle text-lg font-bold">
+              {title}
+            </Link>
+          </div>
+          <div className="flex-1 px-2 mx-2">
+            <div className="flex justify-end">
+              <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+                Home
+              </Link>
+              <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+                About
+              </Link>
             </div>
-            <div className="flex-1 px-2 mx-2">
-                <div className="flex justify-end">
-                    <Link to='/'>Home</Link>
-                </div>
-            </div>
+          </div>
         </div>
       </nav>
     );

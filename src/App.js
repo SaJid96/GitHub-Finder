1cf3dfefd24475919beacd61588ede748ca186c3
Sajid Hussain <sajidhussain98@gmail.com>
2022-01-06 23:18:15 +0530

    added Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Route } from "react-router-dom";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import About from "./pages/About";
@@ -12,7 +12,23 @@ function App() {
         <Navbar />
         <main
         className="container  mx-auto px-3 pb-12"
-        >content</main>
+        >
+
+          <Routes>
+
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/notfound" element={<NotFound/>}/>
+          <Route path="/*" element={<NotFound/>}/>
+
+
+
+
+          </Routes>
+
+
+
+        </main>
         <Footer/>
       </div>
     </Router>

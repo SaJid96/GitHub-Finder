b53646fb0bd6a499d5b35904161a27d95b99751c
Sajid Hussain <sajidhussain98@gmail.com>
2022-01-08 22:58:32 +0530

    Search Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import UserResults from '../components/users/UserResults';
-
+import UserSearch from '../components/users/UserSearch';
 function Home() {
     return (
       <div>
-
-        {/* Seacrch component */}
-        <h1 className="text-6xl"><UserResults/></h1>
+        <UserSearch/>
+        
+          <UserResults />
       </div>
     );
 }

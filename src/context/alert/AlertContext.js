620601a335022ef4b6b64714d8be57c507f0edfd
Sajid Hussain <sajidhussain98@gmail.com>
2022-01-10 22:34:46 +0530

    Alert Context and Reducer

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,48 @@
+import { createContext, useReducer } from 'react';
+
+import alertReducer from './AlertReducer';
+
+
+const AlertContext= createContext()
+
+//provider
+
+export const AlertProvider=({children})=>{
+
+    const intialState=null
+
+    const [state,dispatch]=useReducer(alertReducer,intialState)
+
+    //set an Alert
+
+    const setAlert= (msg,type) =>{
+
+        dispatch({
+            type:'SET_ALERT',
+            payload:{msg,type}
+        })
+    }
+
+    setTimeout(()=>{
+
+        dispatch({
+            type:'REMOVE_ALERT'
+        })
+
+    },3000)
+
+
+    return <AlertContext.Provider
+    
+    value={{alert:state,setAlert}}
+    
+    >
+
+        {children}
+    </AlertContext.Provider>
+
+
+
+}
+
+export default AlertContext
